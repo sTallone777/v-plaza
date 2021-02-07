f81069542611d68d4519b1127ddae7aac8efa617
ranbo <wanglanbo123@icloud.com>
2021-02-07 11:22:45 +0900

    add datasource

--- vplaza/vplazasvr/vplaza-api/src/main/java/com/ranbo/vplaza/api/service/TestService.java
+++ vplaza/vplazasvr/vplaza-api/src/main/java/com/ranbo/vplaza/api/service/TestService.java
@@ -1,6 +1,9 @@
 package com.ranbo.vplaza.api.service;
 
 import com.ranbo.vplaza.annotation.ExecTimeLog;
+import com.ranbo.vplaza.db.mapper.MAdminPermissionMapper;
+import com.ranbo.vplaza.db.model.MAdminPermission;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,6 +11,9 @@ import java.util.List;
 
 @Service
 public class TestService {
+    @Autowired
+    MAdminPermissionMapper adminMapper;
+
     @ExecTimeLog
     public List<String> getUserList(){
         List<String> userList = new ArrayList<>();
@@ -15,5 +21,10 @@ public class TestService {
         userList.add("user2");
         userList.add("user3");
         return userList;
+    }
+
+    public int addAdmin(MAdminPermission adminPermission){
+        int result = adminMapper.insertSelective(adminPermission);
+        return result;
     }
 }
--- vplaza/vplazasvr/vplaza-app/src/main/java/com/ranbo/vplaza/app/controller/testController.java
+++ vplaza/vplazasvr/vplaza-app/src/main/java/com/ranbo/vplaza/app/controller/testController.java
@@ -2,6 +2,7 @@ package com.ranbo.vplaza.app.controller;
 
 import com.ranbo.vplaza.annotation.ExecTimeLog;
 import com.ranbo.vplaza.api.service.TestService;
+import com.ranbo.vplaza.db.model.MAdminPermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +18,14 @@ public class testController {
     @ExecTimeLog
     public String method1(){
         return ts.getUserList().toString();
+    }
+
+    @ExecTimeLog
+    @GetMapping("/addAdmin")
+    public int method2(){
+        MAdminPermission admin = new MAdminPermission();
+        admin.setPermissionCode((byte)(1&0xFF));
+        admin.setPermissionName("super admin");
+        return ts.addAdmin(admin);
     }
 }
